41a9db84eadbdac235d4b037e9eaa655af7d71cd
Marcin Rompczyk <marcinr@stud.ntnu.no>
2025-05-25 20:16:05 +0200

    added get_tcp_pos service to robot_moveit.cpp

--- src/robot_moveit/src/robot_moveit.cpp
+++ src/robot_moveit/src/robot_moveit.cpp
@@ -1,8 +1,10 @@
 #include <memory>
+#include "staticPositions.cpp"
 #include "Static_pos.hpp"
 
 #include <moveit/move_group_interface/move_group_interface.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <object_reference_msg/srv/object_reference.hpp>
 
 // Source link: https://moveit.picknik.ai/main/doc/tutorials/visualizing_in_rviz/visualizing_in_rviz.html
 
@@ -27,6 +29,24 @@ bool plan_and_execute(
   }
 }
 
+double target_x, target_y;
+
+void add(const std::shared_ptr<object_reference_msg::srv::ObjectReference::Request> request,
+          std::shared_ptr<object_reference_msg::srv::ObjectReference::Response>       response)
+{
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\nx: %f" " y: %f",
+                request->x, request->y);
+  if (0.1 <= request->x && request->x <= 1.00 && -0.1 <= request->y  && request->y <= 0.5) {
+    target_x = request->x;
+    target_y = request->y;
+    response->success = true;
+  } else {
+    response->success = false;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request failed!");
+  }
+
+}
+
 int main(int argc, char * argv[])
 {
   // Initialize ROS and create the node
@@ -40,14 +60,18 @@ int main(int argc, char * argv[])
   using moveit::planning_interface::MoveGroupInterface;
   MoveGroupInterface move_group(node, "ur_manipulator");
 
+  rclcpp::Service<object_reference_msg::srv::ObjectReference>::SharedPtr service =
+   node->create_service<object_reference_msg::srv::ObjectReference>("get_tcp_pos",  &add);
 
 
-  plan_and_execute(move_group, home_joint_values, logger, "home position");
-
+  plan_and_execute(move_group, coordinatesForHome, logger, "home position");
 
 
   plan_and_execute(move_group, ref_joint_values, logger, "reference position");
 
+
+
+  rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
