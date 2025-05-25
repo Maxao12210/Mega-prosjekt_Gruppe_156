cce6523b9eb4e4011309972eb341fc47845f7cb8
mdasss <143505645+mdasss@users.noreply.github.com>
2025-05-25 17:06:59 +0200

    Add files via upload

--- src/robot_moveit/include/staticPositions.cpp
+++ src/robot_moveit/include/staticPositions.cpp
@@ -6,11 +6,10 @@
 
 int main() {
 
-    staticPositions homePosition("Home position", 0.0,-1.571,-0,0.0,0.0,0.0);
-    homePosition.getCoordinatesMap();
+    staticPositions homePosition("Home position", 0.000,-1.571,-0.000,0.000,0.000,0.000);
+    std::map<std::string, double> coordinatesForHome = homePosition.getCoordinatesMap();
 
     staticPositions ref1("Position nr.1", 0.005, -1.060, 0.3875, -0.930, -1.474,-0.0005);
-    ref1.getCoordinatesMap();
-
+    std::map<std::string, double> coordinatesForRef1 = ref1.getCoordinatesMap();
     return 0;
 }
