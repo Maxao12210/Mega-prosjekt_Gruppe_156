dbd841dde90a521c1e298ca4eba31fb08ec262eb
Marcin Rompczyk <marcinr@stud.ntnu.no>
2025-05-25 13:39:17 +0200

    functioning code before "clean"

--- src/robot_moveit/src/robot_moveit.cpp
+++ src/robot_moveit/src/robot_moveit.cpp
@@ -20,15 +20,15 @@ int main(int argc, char * argv[])
   auto move_group_interface = MoveGroupInterface(node, "ur_manipulator");
 
   // Create a map of joint names to target values (in radians or meters)
-  std::map<std::string, double> target_joint_values;
-  target_joint_values["shoulder_lift_joint"] = -1.571; // radians
-  target_joint_values["elbow_joint"] = -0.000;
-  target_joint_values["wrist_1_joint"] = 0.000;
-  target_joint_values["wrist_2_joint"] = 0.000;
-  target_joint_values["wrist_3_joint"] = 0.000;
-  target_joint_values["shoulder_pan_joint"] = 0.000;
+  std::map<std::string, double> home_joint_values;
+  home_joint_values["shoulder_lift_joint"] = -1.571; // radians
+  home_joint_values["elbow_joint"] = -0.000;
+  home_joint_values["wrist_1_joint"] = 0.000;
+  home_joint_values["wrist_2_joint"] = 0.000;
+  home_joint_values["wrist_3_joint"] = 0.000;
+  home_joint_values["shoulder_pan_joint"] = 0.000;
 
-  move_group_interface.setJointValueTarget(target_joint_values);
+  move_group_interface.setJointValueTarget(home_joint_values);
   
   // Create a plan to that target pose
   auto const [success, plan] = [&move_group_interface]{
@@ -44,6 +44,30 @@ int main(int argc, char * argv[])
     RCLCPP_ERROR(logger, "Planning failed!");
   }
 
+  // Create a map of joint names to target values (in radians or meters)
+  std::map<std::string, double> refPos;
+  refPos["shoulder_lift_joint"] = -1.060; // radians
+  refPos["elbow_joint"] = 0.3875;
+  refPos["wrist_1_joint"] = -0.930;
+  refPos["wrist_2_joint"] = -1.474;
+  refPos["wrist_3_joint"] = -0.0005;
+  refPos["shoulder_pan_joint"] = 0.005;
+
+  move_group_interface.setJointValueTarget(refPos);
+
+  // Create a plan to that target pose
+  auto const [success2, plan2] = [&move_group_interface]{
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
+
+  // Execute the plan
+  if(success2) {
+    move_group_interface.execute(plan2);
+  } else {
+    RCLCPP_ERROR(logger, "Planning failed!");
+  }
   // Shutdown ROS
   rclcpp::shutdown();
   return 0;
