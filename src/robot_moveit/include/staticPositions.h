89be86ad4b0e54bfde3e10efa8de01695f618a15
mdasss <143505645+mdasss@users.noreply.github.com>
2025-05-25 16:06:08 +0200

    staticPosition class

---
+++ src/robot_moveit/include/staticPositions.h
@@ -0,0 +1,34 @@
+//
+// Created by drinkalotofwater on 5/25/25.
+//
+
+#ifndef STATICPOSITIONS_H
+#define STATICPOSITIONS_H
+#include <iostream>
+
+
+
+class staticPositions  {
+    public:
+        double shoulderPanJoint = 0.0;
+        double shoulderLiftJoint = 0.0;
+        double elbowJoint = 0.0;
+        double wrist1Joint = 0.0;
+        double wrist2Joint = 0.0;
+        double wrist3Joint = 0.0;
+
+    void changeValues (double shoulderPan, double shoulderLift, double elbow, double wrist1, double wrist2, double wrist3){
+        shoulderPanJoint = shoulderPan;
+        shoulderLiftJoint = shoulderLift;
+        elbowJoint = elbow;
+        wrist1Joint = wrist1;
+        wrist2Joint = wrist2;
+        wrist3Joint = wrist3;
+
+        std::cout<<"The new valus are: \n" << shoulderPanJoint << "\n" << shoulderLiftJoint << "\n" << elbowJoint << "\n" << wrist1Joint << "\n" << wrist2Joint << "\n" << wrist3Joint << std::endl;
+    }
+};
+
+
+
+#endif //STATICPOSITIONS_H
