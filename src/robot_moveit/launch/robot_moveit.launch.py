152c34f53e756c9e033f550853662537c157a437
Marcin Rompczyk <marcinr@stud.ntnu.no>
2025-05-29 15:45:07 +0200

    robot_moveit launch good-ish

--- src/robot_moveit/launch/robot_moveit.launch.py
+++ src/robot_moveit/launch/robot_moveit.launch.py
@@ -4,15 +4,15 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # Create and return the LaunchDescription object
     
-     robot_moveit_node = Node(   
-            package='<robot_moveit>',
-            executable='<robot_moveit.cpp>',
-            name='<robot_moveit>',
-            namespace='<namespace>',  
+     robot_moveit_node= Node(
+            package='robot_moveit',
+            executable='robot_moveit',
+            name='robot_moveit',
+            namespace='namespace',
             output='screen',          
-            parameters=[{'param_name': 'param_value'}],  # change based on the coordinates taken from camera files
-            remappings=[('/old_topic', '/new_topic')]    # 
+            #parameters=[{'param_name': 'param_value'}],  # change based on the coordinates taken from camera files
+            #remappings=[('/old_topic', '/new_topic')]    #
         ),
     
     
-     return LaunchDescription([robot_moveit_node])
+     return LaunchDescription(robot_moveit_node)
